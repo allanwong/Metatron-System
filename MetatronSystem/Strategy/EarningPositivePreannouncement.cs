a10433c769a6709154d6c11b05586334524c0ae1
sagemetatron <yanzhehansyang@outlook.com>
2016-09-02 00:26:06 +0800

    [NEW] 新增业绩预增策略
    Signed-off-by: sagemetatron <yanzhehansyang@outlook.com>

---
+++ MetatronSystem/Strategy/EarningPositivePreannouncement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Connection;
+using WAPIWrapperCSharp;
+
+namespace Strategy
+{
+    class EarningPositivePreannouncement
+    {
+        // 2015-12-09 海通 《潜伏利好之业绩预增》
+        // 筛选条件：
+        // 1. 净利润下限》=1000万
+        // 2. 单季净利润下限》=1000万
+        // 3. 净利润增速下限》=30%
+        // 4. 单季净利润增速下限》=30%
+
+        /// <summary>
+        /// 预告净利润下限
+        /// </summary>
+        /// <param name="strWindCode"></param>
+        /// <param name="dtReportDate"></param>
+        public static void windProfitNoticeNetProfitMin(String strWindCode, DateTime dtReportDate)
+        {
+            WindData wd = ConnWindData.fetchTimeSeriesSecInfo(strWindCode, "profitnotice_netprofitmin", dtReportDate, dtReportDate);
+        }
+
+        /// <summary>
+        /// 预告净利润同比增长下限
+        /// </summary>
+        /// <param name="strWindCode"></param>
+        /// <param name="dtReportDate"></param>
+        public static void windProfitNoticeChangeMin(String strWindCode, DateTime dtReportDate)
+        {
+            WindData wd = ConnWindData.fetchTimeSeriesSecInfo(strWindCode, "profitnotice_changemin", dtReportDate, dtReportDate);
+        }
+    }
+}
